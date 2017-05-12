acd630cbd0dcb34901f45fa41aecd04e780a3e29
Neo <neochong@gmail.com>
2017-05-12 13:07:04 +0800

    final

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = module.exports = express();
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/views'))
 
